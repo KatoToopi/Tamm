812101d1844d4bd35fcf0fefe99ed2685a56b883
Amr <amr.reda151@gmail.com>
2016-08-23 11:59:35 +0200

    test

--- app/src/main/java/com/mobileaders/tamm/activities/RegistrationActivity.java
+++ app/src/main/java/com/mobileaders/tamm/activities/RegistrationActivity.java
@@ -7,9 +7,16 @@ import com.mobileaders.tamm.R;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private void setUpViewsReferences() {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registeration);
+
+        setUpViewsReferences();
+
     }
 }
---
+++ app/src/main/java/com/mobileaders/tamm/utilties/UtilitiesClass.java
@@ -0,0 +1,7 @@
+package com.mobileaders.tamm.utilties;
+
+/**
+ * Created by Amr on 8/23/2016.
+ */
+public class UtilitiesClass {
+}
